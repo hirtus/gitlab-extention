9e4cc054e39848fb5a30904368acac9c439f7ed3
mamedov.r9657@gmail.com <mamedov.r9657@gmail.com>
2021-02-11 15:07:39 +0600

    fix: Исправил баг с пустым значением maxWeight при первом запуске расширения.

--- scripts/boards.js
+++ scripts/boards.js
@@ -1,8 +1,19 @@
+let maxWeight;
+
+chrome.storage.sync.get({
+    weight: ''
+  }, function(items) {
+    maxWeight = items.weight;
+    if(maxWeight === '') maxWeight = 10; 
+  });
+
 function getCardTitleInfo(title) {
     let regex = /(?<title>.*)\[ *(?<weight>[0-9].*) *\](?<tail>.*)/;
     let match = title.match(regex);
-    if (match)
-        return {title: match.groups.title, weight: parseInt(match.groups.weight), tail: match.groups.tail};
+    if (match){
+        let weight = parseInt(match.groups.weight);
+        return {title: match.groups.title, weight: weight > maxWeight ? maxWeight : weight, tail: match.groups.tail};
+    }
     else
         return {title: title, weight: 0, tail: ''};
 }
